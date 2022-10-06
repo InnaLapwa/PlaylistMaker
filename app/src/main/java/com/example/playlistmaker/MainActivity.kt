d77ee93fed3d780cb5f93afb1878ad06c0dfacac
InnaLapwa <inna27.02@yandex.ru>
2022-10-06 22:18:53 +0500

    xml of main and settings screens

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.button_search)
+        searchButton.setOnClickListener {
+            val displaySearch = Intent(this, SearchActivity::class.java)
+            startActivity(displaySearch)
+        }
+
+        val libraryButton = findViewById<Button>(R.id.button_library)
+        libraryButton.setOnClickListener {
+            val displayLibrary = Intent(this, LibraryActivity::class.java)
+            startActivity(displayLibrary)
+        }
+
+        val settingsButton = findViewById<Button>(R.id.button_settings)
+        settingsButton.setOnClickListener {
+            val displaySettings = Intent(this, SettingsActivity::class.java)
+            startActivity(displaySettings)
+        }
     }
 }
