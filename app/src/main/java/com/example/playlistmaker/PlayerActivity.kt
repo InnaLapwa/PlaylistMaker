572d545169dc7f11c7a80d6c519c17aa8cb52f52
InnaLapwa <inna27.02@yandex.ru>
2023-02-11 16:10:27 +0500

    deleted wild-imports

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -9,7 +9,7 @@ import androidx.constraintlayout.widget.Group
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Locale
 
 class PlayerActivity : AppCompatActivity() {
 
