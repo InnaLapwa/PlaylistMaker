7031c37be102f4bce8ec8b2e6ea3ebb40459c024
InnaLapwa <inna27.02@yandex.ru>
2023-04-17 18:31:38 +0500

    PlayerManagerImpl to data

--- app/src/main/java/com/example/playlistmaker/creator/Creator.kt
+++ app/src/main/java/com/example/playlistmaker/creator/Creator.kt
@@ -1,6 +1,6 @@
 package com.example.playlistmaker.creator
 
-import com.example.playlistmaker.domain.impl.PlayerManagerImpl
+import com.example.playlistmaker.data.PlayerManagerImpl
 import com.example.playlistmaker.domain.models.Track
 import com.example.playlistmaker.presentation.player.PlayerPresenter
 import com.example.playlistmaker.presentation.player.PlayerView
--- app/src/main/java/com/example/playlistmaker/data/PlayerManagerImpl.kt
+++ app/src/main/java/com/example/playlistmaker/data/PlayerManagerImpl.kt
@@ -1,4 +1,4 @@
-package com.example.playlistmaker.domain.impl
+package com.example.playlistmaker.data
 
 import android.media.MediaPlayer
 import com.example.playlistmaker.domain.PlayerManager
